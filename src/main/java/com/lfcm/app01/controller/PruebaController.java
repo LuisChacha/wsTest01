5696da70004b83e231513cc66081979a7be60da2
Luis Chacha <luischacha82@gmail.com>
2024-07-14 14:46:46 -0500

    Agregar método: pedirnombre

--- src/main/java/com/lfcm/app01/controller/PruebaController.java
+++ src/main/java/com/lfcm/app01/controller/PruebaController.java
@@ -2,6 +2,7 @@
 package com.lfcm.app01.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class PruebaController {
     @GetMapping
     public String TestService(){
         return "Test ok del servicio";
+    }
+    
+    @GetMapping ("/pedirnombre/{nombre}")
+    public String PedirNombre(@PathVariable String nombre){
+        return "Hola: " + nombre;
     }
     
 }
